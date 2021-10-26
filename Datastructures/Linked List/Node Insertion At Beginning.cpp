ffea8e898d9e748c5b11d129e78b9948cd17ff58
Bhaswati Roy <78029145+BhaswatiRoy@users.noreply.github.com>
2021-10-26 12:42:26 +0530

    Node Insertion At Beginning

---
+++ Datastructures/Linked List/Node Insertion At Beginning.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+struct node
+{
+	int data;
+	node *link;
+};
+
+node *head=NULL;
+
+void insertbeginning(int data)
+{
+	node *temp=new node();
+	temp->data=data;
+	//node inserted at beginning points to next node
+	temp->link=head;
+	//node inserted at beginning will act as head node now
+	head=temp;
+}
+
+void print()
+{
+	node *temp=head;
+	cout<<"Linked List is:";
+	while(temp!=NULL)
+	{
+		cout<<temp->data<<" ";
+		temp=temp->link;
+	}
+	cout<<endl;
+}
+
+int main()
+{
+	int n,data;
+	cout<<"Enter the number of node:";
+	cin>>n;
+	for(int i=0;i<n;i++)
+	{
+		cout<<"Enter value at "<<(n-i)<<" node:";
+		cin>>data;
+		insertbeginning(data);
+	}
+	print();
+}
