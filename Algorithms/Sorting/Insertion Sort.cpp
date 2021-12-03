824d825600c29c07f35041b51c16b5fc36a4124e
Bhaswati Roy <78029145+BhaswatiRoy@users.noreply.github.com>
2021-12-03 13:43:39 +0530

    Add files via upload

---
+++ Algorithms/Sorting/Insertion Sort.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+void selectionsortasec(vector<int>&v)
+{
+	int key,j=0;
+	for(int i=0;i<v.size();i++)
+	{
+		//storing the current value in temporary variable key
+		key=v[i];
+		j=i-1;
+		while(j>=0 && v[j]>key)
+		{
+			//swap values
+			v[j+1]=v[j];
+			j--;
+		}
+		//j is decremented and then value is stored
+		v[j+1]=key;
+	}
+}
+
+void selectionsortdesc(vector<int>&v)
+{
+	int key,j=0;
+	for(int i=0;i<v.size();i++)
+	{
+		//storing the current value in temporary variable key
+		key=v[i];
+		j=i-1;
+		while(j>=0 && v[j]<key)
+		{
+			//swap values
+			v[j+1]=v[j];
+			j--;
+		}
+		//j is decremented and then value is stored
+		v[j+1]=key;
+	}
+}
+
+int main()
+{
+	vector<int>v={34,1,25,67,89,16};
+	cout<<"Unsorted form is:";
+	for(int i=0;i<v.size();i++)
+	{
+		cout<<v[i]<<" ";
+	}
+	cout<<endl;
+	selectionsortasec(v);
+	cout<<"Asecending Order Sorted form is:";
+	for(int i=0;i<v.size();i++)
+	{
+		cout<<v[i]<<" ";
+	}
+	cout<<endl;
+	selectionsortdesc(v);
+	cout<<"Descending Order Sorted form is:";
+	for(int i=0;i<v.size();i++)
+	{
+		cout<<v[i]<<" ";
+	}
+}
