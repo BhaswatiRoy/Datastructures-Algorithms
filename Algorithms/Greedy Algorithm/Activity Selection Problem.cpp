3d7f718b21e134be20078c98546cc0bdb852bd9c
Bhaswati Roy <78029145+BhaswatiRoy@users.noreply.github.com>
2021-12-09 19:33:11 +0530

    Update Activity Selection Problem.cpp

--- Algorithms/Greedy Algorithm/Activity Selection Problem.cpp
+++ Algorithms/Greedy Algorithm/Activity Selection Problem.cpp
@@ -12,12 +12,12 @@ Assume that a person can only work on a single activity at a time
 int main() 
 {
 	vector<vector<int>>v={{10,20},{12,15},{20,30}};
-	int count=1,end=v[0][1];
 	//sorting a 2d vector on basis of the 2nd value in each element
 	sort(v.begin(),v.end(),[&](vector<int>&a,vector<int>&b)
 	{
 		return a[1]<b[1];
 	});
+	int count=1,end=v[0][1];
 	//since we considered v[0][1] for comparing so we need not to take that anymore
 	for(int i=1;i<v.size();i++)
 	{
