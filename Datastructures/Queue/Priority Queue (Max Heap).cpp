c87516fa84c1a17fdc94abe4f9e11351f6596ff1
Bhaswati Roy <roybhaswati24@gmail.com>
2022-02-08 21:17:50 +0530

    Add files via upload

---
+++ Datastructures/Queue/Priority Queue (Max Heap).cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+	//Max Heap in STL
+	priority_queue<int,vector<int>>pqmax;
+	pqmax.push(2);
+	pqmax.push(3);
+	pqmax.push(1);
+	cout<<"Max Heap:"<<endl;
+	//max heap keeps highest/maximum element on top/front
+	cout<<"Top Element:"<<pqmax.top()<<endl;
+	//pop operation of queue happens from top/front
+	pqmax.pop();
+	cout<<"After Popping Top Element:"<<pqmax.top()<<endl;
+	
+	cout<<endl;
+}
