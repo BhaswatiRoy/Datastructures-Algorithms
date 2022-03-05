26c870b7a87369934a1b65e13d928c04759c319e
Bhaswati Roy <roybhaswati24@gmail.com>
2022-03-05 19:01:08 +0530

    Add files via upload

---
+++ Algorithms/Bit Manipulation/Right Shift.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int n,x;
+	cin>>n>>x;
+	int leftshift=(n>>x);
+	cout<<"Left Shift Operation "<<n<<" >> "<<x<<" = "<<leftshift<<endl;
+}
