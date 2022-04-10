0d4d298cac8e523a75883a8469b345098c0a68eb
Bhaswati Roy <roybhaswati24@gmail.com>
2022-04-10 10:45:35 +0530

    Update All Possible Subsequences.cpp

--- Datastructures/Array Or Vector/All Possible Subsequences.cpp
+++ Datastructures/Array Or Vector/All Possible Subsequences.cpp
@@ -5,8 +5,8 @@ vector<int>ds;
 
 void printallsubsequence(int idx,vector<int>&arr,int n)
 {
-	//base case is hit when we have ind as n
-	if(idx==n)
+	//base case is hit when we have ind as equal or more than n
+	if(idx>=n)
 	{
 		for(auto it=ds.begin();it!=ds.end();it++)
 		{
@@ -18,8 +18,11 @@ void printallsubsequence(int idx,vector<int>&arr,int n)
 	
 	//take or pick the particular index (element added to subsequence)
 	ds.push_back(arr[idx]);
+	
 	//since we have completed for ind we will move to ind+1
 	printallsubsequence(idx+1,arr,n);
+	
+	//to not pick the last element which we picked we have to remove then do recursive call
 	ds.pop_back();
 	
 	//not take or not pick the particular index (element not added to subsequence)
