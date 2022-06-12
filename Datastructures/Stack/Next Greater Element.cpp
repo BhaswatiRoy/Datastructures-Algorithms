ea9bf65814d4de3a1b7dbe6dba26647c542dec03
Bhaswati Roy <roybhaswati24@gmail.com>
2022-06-12 12:56:47 +0530

    Update Next Greater Element.cpp

--- Datastructures/Stack/Next Greater Element.cpp
+++ Datastructures/Stack/Next Greater Element.cpp
@@ -6,15 +6,19 @@ vector<long long> nextgreaterelement(vector<long long> arr, int n)
     vector<long long>v(n,0);
     stack<long long>st;
     st.push(-1);
+    //iterate from back
     for(int i=n-1;i>=0;i--)
     {
+	//if top element is more than current element then that is the next greater element    
         if(st.top()>arr[i])
         {
             v[i]=st.top();
             st.push(arr[i]);
         }
+	//otherwise if top element is less than current element    
         else
         {
+	    //iterate and remove elements of stack until we get a greater element or untill stack is empty	
             while(!st.empty())
             {
                 if(st.top()>arr[i])
@@ -28,6 +32,7 @@ vector<long long> nextgreaterelement(vector<long long> arr, int n)
                     st.pop();
                 }
             }
+            //if stack is empty then next greater element is marked as -1
             if(st.empty())
             {
                 v[i]=-1;
