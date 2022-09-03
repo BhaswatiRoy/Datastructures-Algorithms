ac22328d15d03e328bfbc70556bd3a8669b09ec8
Bhaswati Roy <roybhaswati24@gmail.com>
2022-09-03 19:08:56 +0530

    Update Matrix Chain Multiplication.cpp

--- Algorithms/Dynamic Programming/MCM Type/Matrix Chain Multiplication.cpp
+++ Algorithms/Dynamic Programming/MCM Type/Matrix Chain Multiplication.cpp
@@ -14,7 +14,7 @@ int mcmrecursion(vector<int>&arr, int i, int j)
     int minans=INT_MAX;
     //k is the partition point in array which partitions into 2 parts 
     // 1st - i to k, 2nd part - k+1 to j
-	//k also iterated from i to j everytime recursively changing partition point
+    //k also iterated from i to j everytime recursively changing partition point
     for(int k=i;k<j;k++)
     {
         //temporary ans is (left call from i to k) + (right call from k+1 to j) + cost of operation
@@ -32,14 +32,14 @@ int mcmmemoization(vector<int>&arr, int i, int j, vector<vector<int>>&dp)
 		return 0;
 	}
 	if(dp[i][j]!=-1)
-    {
-        return dp[i][j];
-    }
+        {
+                return dp[i][j];
+        }
 	int minans=INT_MAX;
-    for(int k=i;k<j;k++)
-    {
-    	int tempans=mcmmemoization(arr,i,k,dp)+mcmmemoization(arr,k+1,j,dp)+(arr[i-1]*arr[k]*arr[j]);
-    	minans=min(tempans,minans);
+        for(int k=i;k<j;k++)
+        {
+    	        int tempans=mcmmemoization(arr,i,k,dp)+mcmmemoization(arr,k+1,j,dp)+(arr[i-1]*arr[k]*arr[j]);
+    	        minans=min(tempans,minans);
 	}
 	return minans;
 }
@@ -54,6 +54,7 @@ int main()
 	int i=1,j=n-1;
 	int ans1=mcmrecursion(arr,i,j);
 	cout<<"Minimum Cost Of Matrix Chain Multiplication By Recursion:"<<ans1<<endl;
+	//dp vector will be of size dimensions i,j and maximum value of both i and j is n
 	vector<vector<int>>dp(n,vector<int>(n,-1));
 	int ans2=mcmmemoization(arr,i,j,dp);
 	cout<<"Minimum Cost Of Matrix Chain Multiplication By Memoization:"<<ans2<<endl;
