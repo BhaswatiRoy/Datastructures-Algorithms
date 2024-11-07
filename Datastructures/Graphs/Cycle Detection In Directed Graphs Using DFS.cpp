b55c7c38ff31c0b6f6ddf48178fc84b88f11ef47
Bhaswati Roy <roybhaswati24@gmail.com>
2024-11-07 22:23:34 +0000

    Update Cycle Detection In Directed Graphs Using DFS.cpp

--- Datastructures/Graphs/Cycle Detection In Directed Graphs Using DFS.cpp
+++ Datastructures/Graphs/Cycle Detection In Directed Graphs Using DFS.cpp
@@ -16,52 +16,49 @@ Normal DFS will say it's a cycle but it's not. Node has to be revisited for the 
 So we maintain a visited & a path visited array
 */
 
-bool dfscyclecheck(int sourcenode, vector<int>adj[],vector<int>&visited,vector<int>&dfsvisited)
+bool checkcycle(int node, vector<vector<int>>&adj, vector<int>&visited, vector<int>&pathvisited)
 {
-       //marking that node in both vectors as 1
-        visited[sourcenode]=1;
-        dfsvisited[sourcenode]=1;
-        for(auto it=adj[sourcenode].begin();it!=adj[sourcenode].end();it++)
+        visited[node]=1;
+        pathvisited[node]=1;
+        for(int j=0;j<adj[node].size();j++)
         {
-            //if the node is unvisited
-            if(visited[*it]==0)
+            int adjnode=adj[node][j];
+            //if adjnode is not visited run a dfs call on it
+            if(visited[adjnode]==0)
             {
-                //recursively call the function and check for all nodes
-                if(dfscyclecheck(*it,adj,visited,dfsvisited)==true)
+                if(checkcycle(adjnode,adj,visited,pathvisited)==true)
                 {
                     return true;
                 }
             }
-            //if the node was visited(visited[*it]=1) and visited in same directed traversal(dfsvisited[*it]=1)
-            else if(visited[*it]==1 && dfsvisited[*it]==1)
+            //but if it is visited and also visited in current path then cycle
+            else
             {
-                return true;
+                if(pathvisited[adjnode]==1)
+                {
+                    return true;
+                }
             }
         }
-        //if we did not find a cycle in this traversal then we will make that node as 0 in dfsvisited vector 
-	dfsvisited[sourcenode]=0;
-	return false;
+        pathvisited[node]=0;
+        return false;
 }
-bool isCyclic(int V, vector<int> adj[]) 
+
+bool isCyclic(int V, vector<vector<int>> adj) 
 {
-        //marks all nodes that are visited as 1
-        vector<int>visited(V+1,0);
-        //marks nodes that are visited as 1 and when recursion call is returned while going back it again marks as 0
-        //this will keep track if the node was visited in directed traversal
-        vector<int>dfsvisited(V+1,0);
+        //pathvisited to check if the node is visited in current path
+        vector<int>visited(V,0);
+        vector<int>pathvisited(V,0);
         for(int i=0;i<V;i++)
         {
-            //check if the node is unvisited then we will visit that node to do further checks
             if(visited[i]==0)
             {
-		//if there is cycle for any node then there is cycle in entire graph
-                if(dfscyclecheck(i,adj,visited,dfsvisited)==true)
+                if(checkcycle(i, adj, visited, pathvisited)==true)
                 {
                     return true;
                 }
             }
         }
-        //if no node could return true then there is no cycle in entire graph
         return false;
 }
 
