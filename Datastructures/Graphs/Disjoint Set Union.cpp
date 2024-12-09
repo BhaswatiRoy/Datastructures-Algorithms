d0827fd23c8aa21636cc7b2b38139e623950c926
Bhaswati Roy <roybhaswati24@gmail.com>
2024-12-09 10:47:21 +0000

    Create Disjoint Set Union.cpp

---
+++ Datastructures/Graphs/Disjoint Set Union.cpp
@@ -0,0 +1,8 @@
+/*
+Checks whether 2 nodes are in same graph components in O(1) time.
+Useful for dynamic graphs which change frequently. 
+
+It has 2 main parts:
+1. Parent()
+2. Union() -> by rank & by size
+*/
